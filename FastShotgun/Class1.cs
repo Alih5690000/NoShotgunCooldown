b2ded5f78ce70e53ff8e6a234d8224f2edea2d96
Alih5690000 <ffffffaaaaaa320@outlook.com>
2026-05-19 12:46:48 +0000

    Damn that's sick

---
+++ FastShotgun/Class1.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using UnityEngine;
+using HarmonyLib;
+
+[BepInPlugin("me.fastshotgun", "Fast Shotgun", "1.0")]
+public class Plugin : BaseUnityPlugin
+{
+    void Awake()
+    {
+        Logger.LogInfo("Mod loaded!");
+        var harmony = new Harmony("me.fastshotgun");
+        harmony.PatchAll();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            Logger.LogInfo("Pressed F1");
+        }
+    }
+};
+
+[HarmonyPatch(typeof(Shotgun), "Update")]
+class Shotgun_Patch
+{
+    static void Prefix(Shotgun __instance)
+    {
+        Traverse.Create(__instance)
+            .Field("gunReady")
+            .SetValue(true);
+    }
+}
